28dddf25c683f149b700beb8f1275dd87619a75e
PauSGracia <pau.sole@gracia.lasalle.cat>
2026-01-11 17:55:07 +0100

    feat: create lazycolumn for characters

---
+++ app/src/main/java/com/example/marcelfeliu_pausole_projecte/view/LazyColumnCharacters.kt
@@ -0,0 +1,29 @@
+package com.example.marcelfeliu_pausole_projecte.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.marcelfeliu_pausole_projecte.viewmodel.RickAndMortyViewModel
+
+@Composable
+fun LazyColumnCharacters(navController: NavController, viewModel: RickAndMortyViewModel){
+    val characterList by viewModel.characters.observeAsState(mutableListOf())
+
+    LazyColumn(verticalArrangement = Arrangement.spacedBy(4.dp),
+            modifier = Modifier
+                .padding(vertical = 30.dp)
+                .fillMaxHeight()
+        ) {
+        items(characterList){ char ->
+            RMCharacterItem(char)
+        }
+    }
+}
