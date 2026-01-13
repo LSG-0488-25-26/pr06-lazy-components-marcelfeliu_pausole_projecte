384cc4b0533576b5ada55ea4b3a40b7f2beb7a10
Marcel Feliu Saperas <marcel.feliu@gracia.lasalle.cat>
2026-01-13 18:26:39 +0100

    feat: Add title to the list

--- app/src/main/java/com/example/marcelfeliu_pausole_projecte/view/LazyColumnCharacters.kt
+++ app/src/main/java/com/example/marcelfeliu_pausole_projecte/view/LazyColumnCharacters.kt
@@ -1,17 +1,25 @@
 package com.example.marcelfeliu_pausole_projecte.view
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.marcelfeliu_pausole_projecte.R
 import com.example.marcelfeliu_pausole_projecte.model.RMCharacter
@@ -21,16 +29,30 @@ import com.example.marcelfeliu_pausole_projecte.viewmodel.RickAndMortyViewModel
 fun LazyColumnCharacters(navController: NavController, viewModel: RickAndMortyViewModel){
     val characterList by viewModel.characters.observeAsState(mutableListOf())
 
-    LazyColumn(verticalArrangement = Arrangement.spacedBy(4.dp),
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize().safeDrawingPadding()
+    ) {
+        Text(
+            text = "Character List",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        LazyColumn(verticalArrangement = Arrangement.spacedBy(4.dp),
             modifier = Modifier
                 .padding(vertical = 30.dp)
                 .fillMaxHeight()
         ) {
-        items(characterList){  char ->
-            RMCharacterItem(char){
-                viewModel.setCurrentCharacter(char)
-                navController.navigate("DetailScreen")
+            items(characterList){  char ->
+                RMCharacterItem(char){
+                    viewModel.setCurrentCharacter(char)
+                    navController.navigate("DetailScreen")
+                }
             }
         }
     }
+
+
 }
